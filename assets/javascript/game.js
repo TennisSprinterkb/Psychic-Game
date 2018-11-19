500b4bf0f85c5a02267880fd8c69659c2a1b63ff
Prince Avila_b <43385846+TennisSprinterkb@users.noreply.github.com>
2018-11-19 15:40:45 -0500

    PsychicGame

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,17 @@
+//The choices for user and computer to pick
 var choices = ["a","b","c","d","e",
             "f","g","h","i","j","k","l","m",
             "n","o","p","q","r","s","t","u",
             "v","w","x","y","z"];
-            
+
+//Setting variables for tracking players wins, losses and guesses left          
 var Wins = 0;
 var Losses = 0;
-//Guesses = 9;
 var whatsLeft = 9;
 var guessedLetters = [];
 var letterToGuess =  [];
 
+//Fun prompt to get to get th user engaged
 var playGame = confirm("Are you sure you want to enter!?");
     if(playGame) {
         alert("Prepare yourself!");
@@ -17,20 +19,17 @@ var playGame = confirm("Are you sure you want to enter!?");
         alert("Too bad you're playing!!");
         };
 
+//Initial computer choice        
 window.onload =function() {
     var compguess = choices[Math.floor(Math.random()*choices.length)];
         letterToGuess.push(compguess);
                 
-        console.log(letterToGuess[0]);
 }
 
-
+//Game play
 document.onkeyup = function() {
     var userinput = String.fromCharCode(event.keyCode).toLowerCase();
         guessedLetters.push(userinput);
-        
-        console.log(userinput[0]);
-
 
     if (userinput=='a' || userinput=='b' || userinput=='c' ||
         userinput=='d' || userinput=='e' || userinput=='f' ||
@@ -43,35 +42,34 @@ document.onkeyup = function() {
         userinput=='y' || userinput=='z') {
 
     if ((userinput === letterToGuess[0]) && (whatsLeft > 0)) {
-                       Wins++;
-                       whatsLeft = 9;
-                       guessedLetters.length = 0;
-                       letterToGuess.length = 0;
-                       var compguess = choices[Math.floor(Math.random()*choices.length)];
-                        letterToGuess.push(compguess);
-                        console.log(letterToGuess[0]);
-                       alert("wins: " + Wins);
-                   }
+        Wins++;
+        whatsLeft = 9;
+        guessedLetters.length = 0;
+        letterToGuess.length = 0;
+        var compguess = choices[Math.floor(Math.random()*choices.length)];
+        letterToGuess.push(compguess);
+        alert("You are a psychic!! Wins: " + Wins);
+        }
 
-                   else if ((userinput !== letterToGuess[0]) && (whatsLeft > 0)) {
-                       whatsLeft = whatsLeft-1;
-                   }
+    else if ((userinput !== letterToGuess[0]) && (whatsLeft > 0)) {
+        whatsLeft = whatsLeft-1;
+        }
 
-                   else if ((userinput !== compguess)) {
-                       Losses++;
-                       whatsLeft = 9;
-                       guessedLetters.length = 0;
-                       letterToGuess.length = 0;
-                       var compguess = choices[Math.floor(Math.random()*choices.length)];
-                        letterToGuess.push(compguess);
-                       alert("loser: "+ Losses);
-                   }
+    else if ((userinput !== compguess)) {
+        Losses++;
+        whatsLeft = 9;
+        guessedLetters.length = 0;
+        letterToGuess.length = 0;
+        var compguess = choices[Math.floor(Math.random()*choices.length)];
+        letterToGuess.push(compguess);
+        alert("Sorry you lost! Losses: "+ Losses);
+        }
 
-                    }else {
-                        alert("Only hit letters please!");
-                    }
+        }else {
+        alert("Only hit letters please!");
+            }
 
-                var html = "<h5>Using only the letters on the keyboard try to match the letter the computer is guessing.</h5>" +
+    var html = "<h5>Using only the letters on the keyboard try to match the letter the computer is guessing.</h5>" +
                 "<h6>Press any letter to start playing!</h6>" +
                 "<p>wins: " + Wins + "</p>" +
                 "<p>losses: " +Losses + "</p>" +
@@ -79,6 +77,6 @@ document.onkeyup = function() {
                 "<p>Letters used: " + guessedLetters + "</p>";
                 
 
-                document.querySelector('#game').innerHTML = html;
+    document.querySelector('#game').innerHTML = html;
 
         }
