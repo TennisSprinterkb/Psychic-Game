27ae25e44b7d075dbac2d1141fa96c009baaff59
Prince Avila_b <43385846+TennisSprinterkb@users.noreply.github.com>
2018-11-19 12:57:33 -0500

    Homework

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Psychic-Game
-Easy Homework
+This is the game in where you have to be a pyschic and guess the letter the computer is thinking of.
+Don't waste letters, there is not true strategy.
+Good luck welcome to KB Psychic! Good luck!! 
+Homework 3 upenn bootcamp
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,63 +2,66 @@ var choices = ["a","b","c","d","e",
             "f","g","h","i","j","k","l","m",
             "n","o","p","q","r","s","t","u",
             "v","w","x","y","z"];
-        var Wins = 0;
-        var Losses = 0;
-        var Guesses = 9;
-        var whatsLeft = 9;
-        var guessedLetters = [];
-        var letterToGuess =  [];
+            
+var Wins = 0;
+var Losses = 0;
+//Guesses = 9;
+var whatsLeft = 9;
+var guessedLetters = [];
+var letterToGuess =  [];
 
-        var playGame = confirm("Are you sure you want to enter!?");
-
-        if(playGame) {
-            alert("Prepare yourself!");
-        } else {
-            alert("Too bad you're playing!!");
+var playGame = confirm("Are you sure you want to enter!?");
+    if(playGame) {
+        alert("Prepare yourself!");
+    } else {
+        alert("Too bad you're playing!!");
         };
 
+window.onload =function() {
+    var compguess = choices[Math.floor(Math.random()*choices.length)];
+        letterToGuess.push(compguess);
+                
+        console.log(letterToGuess[0]);
+}
 
-        document.onkeyup = function() {
-            var userinput = String.fromCharCode(event.keyCode).
-                toLowerCase();
-                guessedLetters.push(userinput);
 
-            console.log(userinput);
+document.onkeyup = function() {
+    var userinput = String.fromCharCode(event.keyCode).toLowerCase();
+        guessedLetters.push(userinput);
+        
+        console.log(userinput[0]);
 
 
-            var compguess = choices[Math.floor(Math.random()*choices.length)];
-            letterToGuess.push(compguess);
-            
+    if (userinput=='a' || userinput=='b' || userinput=='c' ||
+        userinput=='d' || userinput=='e' || userinput=='f' ||
+        userinput=='g' || userinput=='h' || userinput=='i' ||
+        userinput=='j' || userinput=='k' || userinput=="l" ||
+        userinput=='m' || userinput=='n' || userinput=='o' ||
+        userinput=='p' || userinput=='q' || userinput=='r' ||
+        userinput=='s' || userinput=='t' || userinput=='u' ||
+        userinput=='v' || userinput=='w' || userinput=="x" ||
+        userinput=='y' || userinput=='z') {
 
-            if (userinput=='a' || userinput=='b' || userinput=='c' ||
-                userinput=='d' || userinput=='e' || userinput=='f' ||
-                userinput=='g' || userinput=='h' || userinput=='i' ||
-                userinput=='j' || userinput=='k' || userinput=="l" ||
-                userinput=='m' || userinput=='n' || userinput=='o' ||
-                userinput=='p' || userinput=='q' || userinput=='r' ||
-                userinput=='s' || userinput=='t' || userinput=='u' ||
-                userinput=='v' || userinput=='w' || userinput=="x" ||
-                userinput=='y' || userinput=='z') {
-
-                    if ((userinput === compguess[0]) && (whatsLeft > 0)) {
+    if ((userinput === letterToGuess[0]) && (whatsLeft > 0)) {
                        Wins++;
                        whatsLeft = 9;
                        guessedLetters.length = 0;
-                       compguess.length = 0;
+                       letterToGuess.length = 0;
                        var compguess = choices[Math.floor(Math.random()*choices.length)];
                         letterToGuess.push(compguess);
+                        console.log(letterToGuess[0]);
                        alert("wins: " + Wins);
                    }
 
-                   else if ((userinput !== compguess[0]) && (whatsLeft > 0)) {
+                   else if ((userinput !== letterToGuess[0]) && (whatsLeft > 0)) {
                        whatsLeft = whatsLeft-1;
                    }
 
-                   else if ((userinput!=compguess)) {
+                   else if ((userinput !== compguess)) {
                        Losses++;
                        whatsLeft = 9;
                        guessedLetters.length = 0;
-                       compguess.length = 0;
+                       letterToGuess.length = 0;
                        var compguess = choices[Math.floor(Math.random()*choices.length)];
                         letterToGuess.push(compguess);
                        alert("loser: "+ Losses);
